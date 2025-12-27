f5d58cf08dce147edfd8b2a3dec48a8b723c3f4f
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2025-12-27 21:55:29 +0530

    Dashboard authenticate

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+from blogs.models import Blog, Category
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'dashboards/dashboard.html')
+    categorys_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+    return render(request, 'dashboards/dashboard.html', {
+        'categorys_count': categorys_count,
+        'blogs_count': blogs_count
+    })
