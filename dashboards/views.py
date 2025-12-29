3caa0e00ba36c0365c2bdf41572700ceaa0d3fed
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2025-12-29 22:11:57 +0530

    Functionality - Add Category from Dashboard

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
+
+from .forms import CategoryForm
 
 @login_required(login_url='login')
 def dashboard(request):
@@ -14,8 +16,23 @@ def dashboard(request):
     return render(request, 'dashboards/dashboard.html', context)
 
 @login_required(login_url='login')
-def dashboard_categories(request):
+def categories(request):
    # not required to fetch category because context processor is used
-    return render(request, 'dashboards/dashboard_categories.html')
+    return render(request, 'dashboards/categories.html')
+
+@login_required(login_url='login')  
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    else:
+        form=CategoryForm()
+        context = {
+            'form': form
+        }
+    return render(request, 'dashboards/add_category.html', context)
+        
 
 
