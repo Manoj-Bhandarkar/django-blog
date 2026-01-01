a4606549391aed19a5b1abc14381b27ef441e587
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-01-01 18:50:55 +0530

    Blogs - Comment Feature

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
-from .models import Blog, Category
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Blog, Category, Comment
 from django.db.models import Q
 
 
@@ -24,11 +25,24 @@ def posts_by_category(request, category_id):
     return render(request, "posts_by_category.html", context)
 
 def blog_detail(request, slug):
-	single_blog = get_object_or_404(Blog, slug=slug, status="Published")
-	context = {
-		"single_blog": single_blog,
-	}
-	return render(request, "blog_detail.html", context)
+    single_blog = get_object_or_404(Blog, slug=slug, status="Published")
+    if request.method == "POST":
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST["comment"]
+        comment.save()
+        #return redirect("blog_detail", slug=slug)
+        # or use HttpResponseRedirect
+        # from django.http import HttpResponseRedirect  
+        return HttpResponseRedirect(request.path_info)
+    #comment section
+    comments = Comment.objects.filter(blog=single_blog)
+    context = {
+        "single_blog": single_blog,
+        "comments": comments,
+    }
+    return render(request, "blog_detail.html", context)
 
 def blog_search(request):
     keyword = request.GET.get("keyword")
