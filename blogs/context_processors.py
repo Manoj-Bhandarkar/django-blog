cdc10fd43967108b92b8decaadac6b5271594fe1
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-05-05 20:11:02 +0530

    category available all over

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,16 +1,19 @@
-from dashboards.views import categories
-
 from .models import Category
 from about.models import SocialLink
 from django.core.cache import cache
+from django.db.models import Count, Q
 
 def get_categories(request):
-    categories = cache.get('global_categories')
+    categories = cache.get("global_categories")
     if not categories:
-        categories = list(Category.objects.all().values())
-        cache.set('global_categories', categories, 60 * 60)
-    return dict(categories=categories)
+        categories = Category.objects.annotate(
+            post_count=Count("blogs", filter=Q(blogs__status="published"))
+        ).filter(post_count__gt=0)
+        cache.set("global_categories", categories, 60 * 60)
+    return {"categories": categories}\
 
 def get_social_links(request):
-    social_links = cache.get_or_set('social_links_list', SocialLink.objects.all(), 60 * 60)
-    return dict(social_links=social_links)
+    social_links = cache.get_or_set(
+        "social_links_list", SocialLink.objects.all(), 60 * 60
+    )
+    return {'social_links': social_links}
