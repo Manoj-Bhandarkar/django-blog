bcbc73a3f6cbbb849f4a9b0d523df0d90f338f87
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-05-05 21:26:53 +0530

    404

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
 ALLOWED_HOSTS = ["13.233.184.40", "127.0.0.1","localhost"]
